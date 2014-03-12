b7c9bfa6f070259627e4e94b1f54b3752e4493de
VrijVlinder <adalib@mac.com>
2014-03-12 12:24:58 -0600

    New

---
+++ default.php
@@ -0,0 +1,62 @@
+<?php if (!defined('APPLICATION')) exit();
+
+
+// Define the plugin:
+
+
+$PluginInfo['CustomCursor'] = array(
+   'Name'=>'CustomCursor',
+   'Description' => "This plugin adds a custom cursor.",
+   'Version' => '1.0',
+   'MobileFriendly'=>TRUE,
+   'Author' => "VrijVlinder",
+   'AuthorEmail' => 'contact@vrijvlinder.com',
+   'AuthorUrl' => "http://www.vrijvlinder.com"
+);
+
+class CustomCursor_Plugin extends Gdn_Plugin {
+
+  public function __construct() {
+      
+   }
+
+  
+  
+
+	
+   public function Setup() {
+		
+   }
+
+
+public function Base_Render_Before($Sender) {
+
+$Sender->AddCssFile('plugins/CustomCursor/cc.css');
+}
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     public function OnDisable() {
+      return TRUE;
+   }
+
+}
+
+
+	
+        	   
---
+++ readme.txt
@@ -0,0 +1,10 @@
+This plugin adds a custom cursor which you can add to any element in the body.
+
+You can find more cursors here  http://www.totallyfreecursors.com
+
+http://www.cursors-4u.com
+
+body {
+
+cursor: url(http://cur.cursors-4u.net/cursors/cur-2/cur116.cur),progress;
+}
